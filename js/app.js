77ab599241bb3453107cfd0b02ad03741ab76383
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-29 20:01:01 +0900

    app.jsを追加

---
+++ js/app.js
@@ -0,0 +1,61 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    breakpoints: {
+      // 480px以上の場合
+      480: {
+        slidesPerView: 1,
+      },
+    },
+    loop: true,
+    autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: false,
+		reverseDirection: false
+	},
+    pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true
+    }
+    
+});
+
+$(function(){
+        $('.bikes').hover(function(){
+            $(this).text('バイク');
+        },function(){
+            $(this).text('BIKES');
+        });
+        $('.news').hover(function(){
+            $(this).text('ニュース');
+        },function(){
+            $(this).text('NEWS');
+        });
+        $('.athlete').hover(function(){
+            $(this).text('ビアンキ　アスリート');
+        },function(){
+            $(this).text('BIANCHI AHELETE');
+        });
+        $('.about').hover(function(){
+            $(this).text('ビアンキについて');
+        },function(){
+            $(this).text('ABOUT BIANCHI');
+        });
+        $('.store').hover(function(){
+            $(this).text('ストア');
+        },function(){
+            $(this).text('STORE');
+        });
+        $('.online').hover(function(){
+            $(this).text('ビアンキオンラインストア');
+        },function(){
+            $(this).text('BIANCHI ONLINE STORE');
+        });
+        $('.user').hover(function(){
+            $(this).text('ユーザー登録');
+        },function(){
+            $(this).text('USER');
+        });
+        });
+    
