8336955198086e196b6c3c82083128f8b4b0b2ef
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-11-29 22:19:39 +0900

    JS調整

--- js/app.js
+++ js/app.js
@@ -90,31 +90,31 @@ $(function(){
     });
 
     // articleスワイパー
-    // var swiper = new Swiper('.article-container', {
-    //     slidesPerView: 1,
-    //     spaceBetween: 10,
-    //     // init: false,
-    //     navigation: {
-    //     nextEl: '.swiper-button-next',
-    //     prevEl: '.swiper-button-prev',
-    //     },
-    //     pagination: {
-    //       el: '.swiper-pagination',
-    //       clickable: true,
-    //     },
-    //     breakpoints: {
-    //       '@0.00': {
-    //         slidesPerView: 1,
-    //         spaceBetween: 10,
-    //       },
-    //       '@0.75': {
-    //         slidesPerView: 2,
-    //         spaceBetween: 20,
-    //       },
+    var swiper = new Swiper('.article-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        // init: false,
+        navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        breakpoints: {
+          '@0.00': {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          '@0.75': {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
   
-    //       '@1.50': {
-    //         slidesPerView: 4,
-    //         spaceBetween: 17,
-    //       },
-    //     }
-    //   });
+          '@1.50': {
+            slidesPerView: 4,
+            spaceBetween: 17,
+          },
+        }
+      });
