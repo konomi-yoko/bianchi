6087043587be05efaef2ef7397c2fa0584017ce2
DESKTOP-POF2QSR\小橋川真矢 <decemberfridayes15@gmail.com>
2020-11-29 16:50:13 +0900

    ハンバーガーメニュー追加

--- header.js
+++ header.js
@@ -1,5 +1,11 @@
 var swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
+    breakpoints: {
+      // 480px以上の場合
+      480: {
+        slidesPerView: 1,
+      },
+    },
     loop: true,
     autoplay: {
 		delay: 3000,
@@ -14,3 +20,62 @@ var swiper = new Swiper('.swiper-container', {
     }
     
 });
+
+$(function(){
+        $('.bikes').hover(function(){
+            $(this).text('バイク');
+        },function(){
+            $(this).text('BIKES');
+        });
+        $('.news').hover(function(){
+            $(this).text('ニュース');
+        },function(){
+            $(this).text('NEWS');
+        });
+        $('.athlete').hover(function(){
+            $(this).text('ビアンキ　アスリート');
+        },function(){
+            $(this).text('BIANCHI AHELETE');
+        });
+        $('.about').hover(function(){
+            $(this).text('ビアンキについて');
+        },function(){
+            $(this).text('ABOUT BIANCHI');
+        });
+        $('.store').hover(function(){
+            $(this).text('ストア');
+        },function(){
+            $(this).text('STORE');
+        });
+        $('.online').hover(function(){
+            $(this).text('ビアンキオンラインストア');
+        },function(){
+            $(this).text('BIANCHI ONLINE STORE');
+        });
+        $('.user').hover(function(){
+            $(this).text('ユーザー登録');
+        },function(){
+            $(this).text('USER');
+        });
+
+         // ハンバーガーメニューが押された時
+        $('#toggle').on('click',function(){
+        $(this).toggleClass('on');
+        $(this).addClass();
+        $('.js-drower').toggleClass('on');
+    
+        let isActive = $(this).hasClass('on');
+        toggleDrower(isActive);
+});
+        //ハンバーガーメニューでドロップダウン
+        function toggleDrower(isActive) {
+        if (isActive) {
+            // onになっていた時、メニューを表示
+            $('#drower-bg').fadeIn(600);
+        } else {
+            // onを外した時、メニューを非表示
+            $('#drower-bg').fadeOut(600);
+        }
+            };
+        });
+    
