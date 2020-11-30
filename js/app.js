2a3eb90ee30a307f2677f050d89f2ac701405c02
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2020-11-30 14:25:21 +0900

    articleのJSを移動

--- js/app.js
+++ js/app.js
@@ -98,31 +98,28 @@ $(function(){
 //     });
 
     // articleスワイパー
-    var artswiper = new Swiper('.article-container', {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        // init: false,
-        navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      // init: false,
+      navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        // '@0.00': {
+        480: {
+          slidesPerView: 2,
+          spaceBetween: 20,
         },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
+
+        960: {
+          slidesPerView: 4,
+          spaceBetween: 17,
         },
-        breakpoints: {
-          '@0.00': {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          '@0.75': {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-  
-          '@1.50': {
-            slidesPerView: 4,
-            spaceBetween: 17,
-          },
-        }
-      });
+      }
+    });
